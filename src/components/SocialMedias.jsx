1e535dbecdf89a3baa2593901a44a50b14005517
Marion Lalonde <lalondemarion.perso@gmail.com>
2022-11-05 11:54:25 +0100

    Adding a transition to the MainPage

--- src/components/SocialMedias.jsx
+++ src/components/SocialMedias.jsx
@@ -8,17 +8,17 @@ function SocialMedias() {
  
  /* Make footer responsive - centered by default for small screen due to mobile first tailwind */
 <footer className="p-4 flex flex-wrap justify-center md:flex md:items-center md:justify-between md:p-6 bg-amber-500 w-screen absolute bottom-0 left-0 right-0">
-    <span className="text-sm sm:text-center text-white">© 2022 <a href="https://www.linkedin.com/in/marionlalonde/" class="hover:underline" target='_blank' rel="noreferrer">Lalonde Marion</a> -Personal project website.
+    <span className="text-sm sm:text-center text-white">© 2022 <a href="https://www.linkedin.com/in/marionlalonde/" className="hover:underline" target='_blank' rel="noreferrer">Lalonde Marion</a> -Personal project website.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm  text-white sm:mt-0">
         <li className='ml-4 mr-4'>
-            <a href="https://github.com/Hysilie " target='_blank' class="mr-4 hover:underline md:mr-6" rel="noreferrer"><img src={github} alt='social medial' className='h-6 w-6' /></a>
+            <a href="https://github.com/Hysilie " target='_blank' className="mr-4 hover:underline md:mr-6" rel="noreferrer"><img src={github} alt='social medial' className='h-6 w-6' /></a>
         </li>
         <li className='ml-4 mr-4'>
-            <a href="https://www.linkedin.com/in/marionlalonde/" class="mr-4 hover:underline md:mr-6 " target='_blank' rel="noreferrer"><img src={linkedin} alt='social medial' className='h-6 w-6' /></a>
+            <a href="https://www.linkedin.com/in/marionlalonde/" className="mr-4 hover:underline md:mr-6 " target='_blank' rel="noreferrer"><img src={linkedin} alt='social medial' className='h-6 w-6' /></a>
         </li>
         <li className='ml-4 mr-4'>
-            <a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=lalondemarion.pro@gmail.com" class="mr-4 hover:underline md:mr-6"target='_blank' rel="noreferrer" ><img src={mail} alt='social medial' className='h-6 w-6' /></a>
+            <a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=lalondemarion.pro@gmail.com" className="mr-4 hover:underline md:mr-6"target='_blank' rel="noreferrer" ><img src={mail} alt='social medial' className='h-6 w-6' /></a>
         </li>
     </ul>
 </footer>   
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
-import backgroundHome from '../../src/images/backgroundHome.jpeg'
+import React, {useState, useEffect} from 'react'
 import vector from '../../src/images/vector.svg'
 import '../../src/style/home.css'
 
 
 const Home = () => {
+
+  /* State for the animation of background */
+  const [backgroundID, setBackgroundID] = useState("home1")
+  useEffect(() => {
+    console.log('test lancement')
+/* set a timer to start the siwtch with the next background */
+    const timer = setTimeout(() => {
+      console.log('test changement')
+      setBackgroundID("home2");
+    }, 10000);
+  
+    setInterval(function() {
+      setBackgroundID("home1");
+/* Interval to put the first background */
+    }, 10000)
+    setInterval(function() {
+      setBackgroundID("home2");
+/* And again for the second */
+}, 20000)
+ /* Clear the time */
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  
   return (
-    /* Make a container  */
-    <div className=" bg-slate-50 drop-shadow-md h-4/6 flex  w-screen home" style={{backgroundImage: `url('${backgroundHome}')`}}>
+   
+
+    <div  className=" bg-slate-50 drop-shadow-md h-4/6 flex  w-screen" id={`${backgroundID}`}>
     <div className='vector h-full flex-1' style={{backgroundImage: `url('${vector}')`}}>
           <div className="text-black h-full flex items-center">
             <div className="flex flex-col">
@@ -15,7 +41,6 @@ const Home = () => {
           <span id="under-title" className='text-lg hidden lg:inline m-5'>Discover a great collection of art</span>
           </div></div>
 </div>
-
     </div>
 
   )
